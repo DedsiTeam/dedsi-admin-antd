e4f9fc31d453b5f52aacac9805f981824bd5ec2c
Cohen <1768065921@qq.com>
2025-05-22 10:36:08 +0800

    添加一个登录输入框

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,15 +1,26 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-  <h1>这是个登录页面</h1>
-  <a-button type="primary" @click="loginClick">登录</a-button>
+  <a-form :label-col="{ span: 4 }" :wrapper-col="{ span: 14 }">
+    <a-form-item label="用户名">
+      <a-input v-model="fromData.account" placeholder="请输入用户名" />
+    </a-form-item>
+    <a-form-item label="密码">
+      <a-input v-model="fromData.pwd" type="password" placeholder="请输入密码" />
+    </a-form-item>
+    <a-form-item :wrapper-col="{ offset: 4, span: 16 }">
+      <a-button type="primary" @click="loginClick">登录</a-button>
+    </a-form-item>
+  </a-form>
 </template>
 
 <script setup lang="ts">
+import { reactive } from 'vue'
 import { useRouter } from 'vue-router'
 const router = useRouter()
+
+const fromData = reactive({
+  account: '',
+  pwd: ''
+})
 
 function loginClick() {
   router.push('/')
