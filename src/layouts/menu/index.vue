ef00abc15f983019a33f76a6737c16bb5ff194e9
Cohen <1768065921@qq.com>
2025-05-28 22:07:27 +0800

    菜单与tab的逻辑
    菜单与tab的逻辑

--- src/layouts/menu/index.vue
+++ src/layouts/menu/index.vue
@@ -10,71 +10,52 @@
     />
 </template>
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import AppName from "../appName/index.vue";
+import { useRouter } from "vue-router";
 
 import { menuRouteStores } from '@/pinias/public-api';
 
-const { addDatas } = menuRouteStores();
+const menuRouteStore = menuRouteStores();
+const router = useRouter();
 
+watch(() => menuRouteStore.menuRouteSelected, (newVal) => {
+    selectedKeys.value = [newVal as string];
+    openKeys.value = [newVal as string];
+});
 
 const menuItems = [
 {
-    key: '1',
-    label: 'Option 1',
-    title: 'Option 1',
+    key: '/dashboard',
+    label: 'dashboard',
+    title: 'dashboard',
   },
   {
-    key: '2',
-    label: 'Option 2',
-    title: 'Option 2',
-    children: [
-      {
-        key: '2-3-1',
-        label: 'Option 2-3-1',
-        title: 'Option 2-3-1',
-      },
-      {
-        key: '2-3-2',
-        label: 'Option 2-3-2',
-        title: 'Option 2-3-2',
-        children: [
-          {
-            key: '2-3-2-1',
-            label: 'Option 2-3-2-1',
-            title: 'Option 2-3-2-1',
-          },
-          {
-            key: '2-3-2-2',
-            label: 'Option 2-3-2-2',
-            title: 'Option 2-3-2-2',
-          },
-        ]
-      },
-    ]
+    key: '/demo/1',
+    label: 'demo 1',
+    title: 'demo 1',
   },
   {
-    key: '3',
-    label: 'Option 3',
-    title: 'Option 3',
-    children: [
-      {
-        key: '3-3-2-1',
-        label: 'Option 3-3-2-1',
-        title: 'Option 3-3-2-1',
-      },
-      {
-        key: '3-3-2-2',
-        label: 'Option 3-3-2-2',
-        title: 'Option 3-3-2-2',
-      },
-    ]
+    key: '/demo/12',
+    label: 'demo 12',
+    title: 'demo 12',
+  },
+  {
+    key: '/demo/123',
+    label: 'demo 123',
+    title: 'demo 123',
+  },
+  {
+    key: '/demo/1345',
+    label: 'demo 1345',
+    title: 'demo 1345',
   }
 ];
 const selectedKeys = ref<string[]>([]);
 const openKeys = ref<string[]>([]);
 
 function handleClick(e: any) {
-    addDatas(e.key);
+    menuRouteStore.add(e.key);
+    router.push(e.key);
 }
 </script>
---
+++ src/views/demo.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>Demo {{ route.path }}</h1>
+</template>
+
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+const route = useRoute();
+</script>
